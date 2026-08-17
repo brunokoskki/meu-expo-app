241fb1739f3e804a7cb71c63038252ccd8e7e9da
Bruno Ski <27brunosf@gmail.com>
2026-08-17 12:53:13 -0300

    Arrumei o app para acompanhar as aulas

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Button, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, ScrollView, TextInput } from 'react-native';
 import { useState, useEffect } from 'react';
-import { TextInput } from 'react-native';
 
 export default function App() {
 
@@ -10,106 +9,130 @@ export default function App() {
 
   const fazerLogin = () => {
 
-    if (login == "Gabriel" && senha == "123")
-    {
-      alert("Bem vindo Gabriel")
+    if (login == "Bruno" && senha == "123") {
+      alert("Você receberá sua conta por E-Mail em até 30 anos.");
     }
     else {
-        alert("Deu ruim")
+      alert("Tá errado, coloca os dados certos.");
     }
 
-    
   }
 
-  useEffect(()=>{
-    console.log(login)
-    console.log(senha)
+  useEffect(() => {
+    console.log(login);
+    console.log(senha);
   });
 
   return (
     <View style={styles.container}>
 
-    <ScrollView
-       contentContainerStyle={{
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexGrow: 1
-  }}
-    >  
-      <Image 
-        style={{
-          width:150, 
-          height:80
-        }} 
-        
-        alignItems="center"
-        source={{uri:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiFp8SrcHwyDiHtbxM1DshunmQ96WLrJGfqbfPR7VCrJZZXfqNNw7vZlgR&s=10"}}
-        
-      ></Image>
+      <ScrollView
+        contentContainerStyle={styles.scroll} 
+      >
 
-      <Text
-      style={{
-        alignSelf: 'center',
-        color: 'white'
-      
-      }}
-      >Login</Text>
+        <Text style={styles.title}>
+            IA Gratis
+        </Text>
 
-      <TextInput
-      placeholder="Typer your login"
-      style={{
-        width: 300,
-        borderWidth: 3,
-        borderColor: 'white',
-        borderRadius: 50,
-        margin: 10,
-        backgroundColor: 'rgba(255, 255, 255, 0.26)',
-        color: 'white',
-        textAlign: 'center'
-      }}
+        <Image
+          style={styles.logo}
+          source={{
+            uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5efDWyK3Z2p_8aYGSyDHrAKVG3QtK080a5fvqoBjYBLFrJcrgkmGtMeq2&s=10"
+          }}
+        />
 
-      onChangeText={(textologin) => setLogin(textologin)}
+        <Text style={styles.text}>
+          Faça seu login para ter IA grátis
+        </Text>
 
-        /> 
-      <StatusBar style="auto" />
-    
-    <TextInput
-      placeholder="Typer your password"
-      style={{
-         width: 300,
-        borderWidth: 3,
-        borderColor: 'white',
-        borderRadius: 50,
-        margin: 10,
-        backgroundColor: 'rgba(255, 255, 255, 0.26)',
-        color: 'white',
-        textAlign: 'center'
-      }}
-        onChangeText={(textosenha) => setSenha(textosenha)}
-    /> 
+        <TextInput
+          placeholder="Digite seu login"
+          placeholderTextColor="#bbbbbb"
+          style={styles.input}
+          onChangeText={(textologin) => setLogin(textologin)}
+        />
 
-    <Button
-      color="black"
-      onPress={fazerLogin}
-      style={styles.button}
-      title="Avançar"      
-    />
+        <StatusBar style="auto" />
 
-      <StatusBar style="auto" />
-    
-    </ScrollView>
-    
+        <TextInput
+          placeholder="Digite sua senha"
+          placeholderTextColor="#bbbbbb"
+          style={styles.input}
+          onChangeText={(textosenha) => setSenha(textosenha)}
+        />
+
+        <Button
+          color="gray"
+          onPress={fazerLogin}
+          title="Avançar"
+        />
+
+        <StatusBar style="auto" />
+
+      </ScrollView>
+
     </View>
-
-    
   );
 }
 
 const styles = StyleSheet.create({
+
   container: {
     alignItems: 'center',
-  justifyContent: 'center',
-  flexGrow: 1,
-  backgroundColor: 'black'
+    justifyContent: 'center',
+    flexGrow: 1,
+    backgroundColor: 'black'
   },
+
+  scroll: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexGrow: 1
+  },
+
+  logo: {
+    width: 250,
+    height: 140,
+    borderRadius: 20,
+    marginTop: -15
+  },
+
+  title: {
+  color: 'white',
+  fontSize: 32,
+  fontWeight: 'bold',
+  marginBottom: 20
+  },
+
+  text: {
+    alignSelf: 'center',
+    color: 'white',
+    fontSize: 24,
+    fontWeight: '300',
+    marginBottom: 5,
+    marginTop: 30
+  },
+
+  input: {
+    width: 300,
+    height: 50,
+    borderWidth: 2,
+    borderColor: 'white',
+    borderRadius: 25,
+    margin: 10,
+    paddingHorizontal: 20,
+    backgroundColor: 'rgba(255, 255, 255, 0.15)',
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 16,
+
+    shadowColor: 'white',
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 5
+  }
+
 });
